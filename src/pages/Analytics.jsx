6ed36fd283f4f2a9243fe846dba17d0b447c21d2
prashantbhavsar782 <prashant.bhavsar782@gmail.com>
2026-04-29 12:02:03 +0530

    alerts update

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { db } from "../firebase";
 import { collection, onSnapshot, query } from "firebase/firestore";
-
+console.log("🔥 Analytics Page Loaded");
 import {
   BarChart,
   Bar,
